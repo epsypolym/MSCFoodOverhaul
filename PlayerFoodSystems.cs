b18b761ba6f00a45b23a6a9753bcc14bf0d98004
ajanhallinta <4janhallinta@gmail.com>
2020-03-18 02:47:11 +0200

    Barebone calories calculation.

--- PlayerFoodSystems.cs
+++ PlayerFoodSystems.cs
@@ -5,8 +5,8 @@ namespace FoodOverhaul
 {
     public class PlayerFoodSystems : MonoBehaviour
     {
-        public float CalorieValue;
-        public float ProteinValue;
+        public float CalorieValue = 3000;
+        public float ProteinValue = 91;
         public float GlucoseValue;
         public float CalciumValue;
         public float ZincValue;
@@ -17,6 +17,38 @@ namespace FoodOverhaul
         public float VitaminDValue;
 
         public float ThirstVolume;
+
+        private float PlayerWalkingSpeed = 0;
+        private float TotalDecreaseRate = 1;
+
+        private CharacterController controller;
+
+        void Start()
+        {
+            controller = FoodOverhaul.PLAYER.GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+            UpdateCalories();
+        }
+
+        void UpdateCalories()
+        {
+            if (controller)
+                PlayerWalkingSpeed = controller.velocity.magnitude;
+
+            CalorieValue -= (PlayerWalkingSpeed < 1 ? 1 : PlayerWalkingSpeed) * TotalDecreaseRate * Time.deltaTime;
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(30, 30, 500, 500));
+            GUILayout.Label("Calories: " + CalorieValue);
+            GUILayout.Label("Proteins: " + ProteinValue);
+            GUILayout.Label("PlayerWalkingSpeed: " + PlayerWalkingSpeed);
+            GUILayout.EndArea();
+        }
 
     }
 }
