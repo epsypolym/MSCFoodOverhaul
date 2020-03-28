b847892851756b9bcd6623f411fc0772916f1784
ajanhallinta <4janhallinta@gmail.com>
2020-03-28 20:35:19 +0200

    Advanced MovingMode logic and Calories calculation
    - MovingMode now includes: Sitting, Driving, Swimming, Walking (+ Running and Jumping), Still (standing) and Shower.
    - Calories calculation updated to take account new MovingModes.

--- PlayerFoodSystems.cs
+++ PlayerFoodSystems.cs
@@ -4,12 +4,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using HutongGames.PlayMaker;
 
 namespace FoodOverhaul
 {
     public class PlayerFoodSystems : MonoBehaviour
     {
-        public enum MovingMode { Still, Walking, Sitting, InCar, CarryingShopBag, Sauna, Shower };
+        public enum MovingMode { Still, Walking, Sitting, Driving, CarryingShopBag, Sauna, Shower, Swimming };
         public MovingMode movingMode = MovingMode.Still;
 
         public Nutrition Calorie = new Nutrition()
@@ -42,12 +43,28 @@ namespace FoodOverhaul
 
         private CharacterController controller;
 
+        private FsmBool PlayerInWater; // if Value is true, then swimming
+        private FsmFloat CrouchPosition; // if Value is < 0.3f, then sitting
+        private FsmFloat ShowerDistance; // player distance from shower
+        private Transform ShowerObject; // if gameObject is active and ShowerDistance is < 0.2f, then in shower
+
+        private AxisCarController currentVehicle = null;
+        private Drivetrain currentDrivetrain = null;
+
         void Start()
         {
             // add nutritions to list
             nutritions.Add(Calorie);
-        
+
             controller = FoodOverhaul.PLAYER.GetComponent<CharacterController>(); // get player
+
+            // get fsm variables
+            PlayMakerFSM _Crouch = FoodOverhaul.PLAYER.GetComponents<PlayMakerFSM>().Where(x => x.FsmName == "Crouch").FirstOrDefault() as PlayMakerFSM;
+            PlayerInWater = _Crouch.FsmVariables.FindFsmBool("PlayerInWater");
+            CrouchPosition = _Crouch.FsmVariables.FindFsmFloat("Position");
+            ShowerObject = GameObject.Find("YARD").transform.Find("Building/BATHROOM/Shower/Shower");
+            ShowerDistance = ShowerObject.GetComponents<PlayMakerFSM>().Where(x => x.FsmName == "Distance").FirstOrDefault().FsmVariables.GetFsmFloat("Distance") as FsmFloat;
+
             StartCoroutine(UpdateCalories()); // start calories update loop
         }
 
@@ -56,27 +73,28 @@ namespace FoodOverhaul
             bool finished = false;
             while(!finished)
             {
-                UpdateCalorieValue();
+                // get current vehicle (if any)
+                try
+                {
+                    currentVehicle = controller.transform.root.GetComponent<AxisCarController>();
+                    currentDrivetrain = currentVehicle.GetComponent<Drivetrain>();
+                } catch
+                {
+                    currentVehicle = null;
+                    currentDrivetrain = null;
+                }
+               
+                UpdateCurrentMovingMode(); // update current moving mode
+                UpdateCalorieValues(); // update current calories
+
                 new WaitForSeconds(1); // update frequency in seconds
                 yield return null;
             }
         }
 
-        void UpdateCalorieValue()
+        void UpdateCalorieValues()
         {
             float totalUsedCalories = UnityEngine.Random.Range(132, 147); // standing
-
-            // get player moving speed
-            if (controller)
-                PlayerWalkingSpeed = controller.velocity.magnitude;
-
-            // get player current action (wip)
-            if (PlayMakerGlobals.Instance.Variables.GetFsmBool("PlayerComputer").Value)
-                movingMode = MovingMode.Sitting;
-            else if (PlayMakerGlobals.Instance.Variables.GetFsmBool("PlayerCarControl").Value)
-                movingMode = MovingMode.InCar;
-            else
-                movingMode = MovingMode.Sitting;
 
             // calculate total used calories (per second)
             totalUsedCalories = GetTotalUsedCalories(movingMode, Calorie);
@@ -94,8 +112,20 @@ namespace FoodOverhaul
                 case MovingMode.Sitting:
                     _ogValue = UnityEngine.Random.Range(68, 74);
                     break;
-                case MovingMode.InCar:
+                case MovingMode.Driving:
                     _ogValue = UnityEngine.Random.Range(114, 121);
+                    break;
+                case MovingMode.Swimming:
+                    _ogValue = UnityEngine.Random.Range(391, 406);
+                    break;
+                case MovingMode.Walking:
+                    _ogValue = UnityEngine.Random.Range(176, 184);
+                    break;
+                case MovingMode.Still:
+                    _ogValue = UnityEngine.Random.Range(92, 110);
+                    break;
+                case MovingMode.Shower:
+                    _ogValue = UnityEngine.Random.Range(143, 153);
                     break;
                 default:
                     _ogValue = calorie.amount;
@@ -111,12 +141,68 @@ namespace FoodOverhaul
                 n.AddAmount(value);
         }
 
+        void UpdateCurrentMovingMode()
+        {
+            // get player moving speed
+            if (controller)
+                PlayerWalkingSpeed = controller.velocity.magnitude;
+
+            // Player is Swimming
+            if (PlayerInWater.Value)
+                movingMode = MovingMode.Swimming;
+            else
+            {
+                // Player is Sitting
+                if (CrouchPosition.Value <= 0.3f && currentVehicle == null && currentDrivetrain != null)
+                {
+                    movingMode = MovingMode.Sitting;
+                }
+                // Player is Driving / Sitting in vehicle
+                else if (currentVehicle != null && currentDrivetrain != null)
+                {
+                    // Player is Driving
+                    if (currentDrivetrain.velo > 1)
+                        movingMode = MovingMode.Driving;
+                    // Player is Sitting in vehicle
+                    else
+                        movingMode = MovingMode.Sitting;
+                }
+                else
+                {
+                    if (PlayerWalkingSpeed < 1)
+                    {
+                        // Player is Sitting
+                        if (CrouchPosition.Value <= 0.3f && PlayerWalkingSpeed == 0)
+                            movingMode = MovingMode.Sitting;
+                        // Player is Standing
+                        else
+                            movingMode = MovingMode.Still;
+                    }
+                    // Player is Walking
+                    else
+                        movingMode = MovingMode.Walking;
+                }
+            }
+
+            // Player is in Shower
+            if (ShowerDistance.Value < 0.2f && ShowerObject.gameObject.activeSelf)
+                movingMode = MovingMode.Shower;
+        }
+
         private void OnGUI()
         {
+            string currentVehicleName = "None";
+            if (currentVehicle != null)
+                currentVehicleName = currentVehicle.name;
+
             GUILayout.BeginArea(new Rect(30, 30, 500, 500));
             GUILayout.Label("Calories: " + Calorie.amount);
             GUILayout.Label("Proteins: " + ProteinValue);
             GUILayout.Label("PlayerWalkingSpeed: " + PlayerWalkingSpeed);
+            GUILayout.Label("CurrentVehicle: " + currentVehicleName);
+            GUILayout.Label("PlayerInWater: " + PlayerInWater.Value.ToString());
+            GUILayout.Label("PlayerCrouchPos: " + CrouchPosition.Value.ToString());
+            GUILayout.Label("MovingMode: " + movingMode.ToString());
             GUILayout.EndArea();
         }
     }
